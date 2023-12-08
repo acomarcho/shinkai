bc95e8bbdc6ae184855c52c760dc4ef309f3d76a
acomarcho <acoxstpd@gmail.com>
2023-12-08 23:50:18 +0700

    Feat: Add ability to generate unique filenames

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,12 @@ from color_matcher.io_handler import load_img_file, save_img_file
 from color_matcher.normalizer import Normalizer
 
 import base64
+import uuid
 
 app = Flask(__name__)
 
 @app.route("/")
-def helllo_world():
+def hello_world():
   return "<p>Hello, World!</p>"
 
 @app.route("/image", methods=['POST'])
@@ -17,16 +18,16 @@ def process_image():
   if request.method == 'POST':
     image_file = request.files['image']
 
-    input_file_path = f'./input/{image_file.filename}'
+    input_file_path = f'./input/{uuid.uuid4()}-{image_file.filename}'
     image_file.save(input_file_path)
-    process_image(input_file_path)
+
+    output_image_path = process_image(input_file_path, image_file.filename)
 
     return {
-      "status": "SUCCESS",
-      "image": create_base64_image("./output/res.png")
+      "image": create_base64_image(output_image_path)
     }
 
-def process_image(input_file_path):
+def process_image(input_file_path, original_file_name):
   cm = ColorMatcher()
 
   img_src = load_img_file(input_file_path)
@@ -35,7 +36,10 @@ def process_image(input_file_path):
   img_res = cm.transfer(src=img_src, ref=img_ref, method='default')
   img_res = Normalizer(img_res).uint8_norm()
 
-  save_img_file(img_res, './output/res.png')
+  output_image_path = f'./output/{uuid.uuid4()}-{original_file_name}.png'
+  save_img_file(img_res, output_image_path)
+
+  return output_image_path
 
 def create_base64_image(input_file_path):
   with open(input_file_path, "rb") as image_file:
