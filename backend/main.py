71d4a08cd263cef1804793c77dd0b2fe3cbacec0
acomarcho <acoxstpd@gmail.com>
2023-12-08 23:45:53 +0700

    Feat: Add ability to return image as base64

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from color_matcher import ColorMatcher
 from color_matcher.io_handler import load_img_file, save_img_file
 from color_matcher.normalizer import Normalizer
 
+import base64
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,7 +22,8 @@ def process_image():
     process_image(input_file_path)
 
     return {
-      "status": "SUCCESS"
+      "status": "SUCCESS",
+      "image": create_base64_image("./output/res.png")
     }
 
 def process_image(input_file_path):
@@ -33,6 +36,12 @@ def process_image(input_file_path):
   img_res = Normalizer(img_res).uint8_norm()
 
   save_img_file(img_res, './output/res.png')
+
+def create_base64_image(input_file_path):
+  with open(input_file_path, "rb") as image_file:
+    image_data = image_file.read()
+    base64_encoded = base64.b64encode(image_data).decode('utf-8')
+    return base64_encoded
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
